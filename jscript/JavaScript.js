44175f6656cd95d959b84f5c24b0c03c9fb093b2
jernmed <127987753+jernmed@users.noreply.github.com>
2023-06-15 13:26:42 +0200

    Finalizing

---
+++ jscript/JavaScript.js
@@ -0,0 +1,21 @@
+﻿function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+function toggleMobileMenu2(menu) {
+    menu.classList.toggle('open');
+}
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
