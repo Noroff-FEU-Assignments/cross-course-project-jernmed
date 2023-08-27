e99b323f6e103809df44ec34f0425db1b08d5c6f
Bryan Luis Villafuerte <bryan.villafuerte@inmeta.no>
2023-08-27 15:19:56 +0200

    Refactored code for better semantics and readability. fixed other minor code errors.

--- jscript/JavaScript.js
+++ jscript/JavaScript.js
@@ -5,9 +5,10 @@
 function toggleMobileMenu2(menu) {
     menu.classList.toggle('open');
 }
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -15,7 +16,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
